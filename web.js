c6a4d559c4cb4e6d4e5fc3ec5c93b6577a115e84
Wendy Tan <w888tan@gmail.com>
2013-07-11 03:25:35 +0000

    Updated with index

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require ('fs');
+  var buf = new Buffer (fs.readFileSync("index.html",'utf-8'));
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
