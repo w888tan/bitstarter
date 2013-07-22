9599e94529743d9241730f312616b9577da20536
Wendy <w888tan@gmail.com>
2013-07-22 06:58:49 +0000

    modify

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
+var fs = require ('fs');
+var htmlfile= "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var fs = require ('fs');
-  var buf = new Buffer (fs.readFileSync("index.html",'utf-8'));
-  response.send(buf.toString());
+  
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
 var port = process.env.PORT || 8080;
